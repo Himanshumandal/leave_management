c640b746267b9b342994fdcbfc142cc22e2aabe4
Himanshu Mandal <himanshum214btechcse2025@kccitm.edu.in>
2026-09-15 01:17:57 +0530

    Update header.php

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -37,7 +37,6 @@ $pageTitle = $pageTitle ?? 'Employee Management System';
         rel="stylesheet"
     >
 
-    >
     <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
