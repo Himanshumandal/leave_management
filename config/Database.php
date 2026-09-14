950bd8fb056125d9aed40fcefe7b6a040525f351
Himanshu Mandal <himanshum214btechcse2025@kccitm.edu.in>
2026-09-14 13:39:49 +0530

    Configure TiDB Cloud SSL connection

--- config/Database.php
+++ config/Database.php
@@ -1,53 +1,44 @@
+<?php
 
-<?php 
+class Database
+{
+    private static ?PDO $instance = null;
 
-    class Database{
-        private static ?PDO $instance=null;
-
-        private function __construct() {
-        }
-
-        public static function getInstance(): PDO {
-
-            if(self::$instance === null){
-                  $host =
-                    env(
-                        'DB_HOST',
-                        'localhost'
-                    );
-
-                $dbname =
-                    env(
-                        'DB_NAME',
-                        'leavemanagementsystem'
-                    );
-
-                $username =
-                    env(
-                        'DB_USERNAME',
-                        'root'
-                    );
-
-                $password =
-                    env(
-                        'DB_PASSWORD',
-                        ''
-                    );
-
-
-                $dbs="mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-
-                self::$instance=new PDO($dbs,$username,$password,[
-
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::ATTR_EMULATE_PREPARES => false
-                ]);
-            }
-
-            return self::$instance;
-        }
-
-
+    private function __construct()
+    {
     }
+
+    public static function getInstance(): PDO
+    {
+        if (self::$instance === null) {
+
+            $host = env('DB_HOST', 'localhost');
+            $port = env('DB_PORT', '3306');
+            $dbname = env('DB_NAME', 'leavemanagementsystem');
+            $username = env('DB_USERNAME', 'root');
+            $password = env('DB_PASSWORD', '');
+
+            $dbs = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+
+                // TiDB Cloud SSL/TLS
+                PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/certs/ca.pem',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+            ];
+
+            self::$instance = new PDO(
+                $dbs,
+                $username,
+                $password,
+                $options
+            );
+        }
+
+        return self::$instance;
+    }
+}
 ?>
